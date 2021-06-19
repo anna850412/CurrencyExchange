191771d24c7a7697a12793a5203cd3f9c26cd7e5
anna850412 <73561196+anna850412@users.noreply.github.com>
2021-06-19 17:53:28 +0200

    plus and minus added to quantity without proper methods

--- src/main/java/com/kodilla/restaurantfrontend/domain/ProductForm.java
+++ src/main/java/com/kodilla/restaurantfrontend/domain/ProductForm.java
@@ -3,6 +3,8 @@ package com.kodilla.restaurantfrontend.domain;
 import com.kodilla.restaurantfrontend.OrderMainView;
 import com.kodilla.restaurantfrontend.ProductType;
 import com.kodilla.restaurantfrontend.service.ProductService;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.InputEvent;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -15,6 +17,8 @@ public class ProductForm extends FormLayout {
     private TextField productName = new TextField("Product name");
     private TextField price = new TextField("Price");
     private TextField quantity = new TextField("Quantity");
+    private Button plus = new Button("+");
+    private Button minus = new Button("-");
     private ComboBox<ProductType> type = new ComboBox<>("Product type");
     private Button order = new Button("Order");
     private Button delete = new Button("Delete");
@@ -28,9 +32,23 @@ public class ProductForm extends FormLayout {
         order.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         order.addClickListener(event -> order());
         delete.addClickListener(event -> delete());
-        add(productName, price, quantity, type, buttons);
+        HorizontalLayout plusAndMinus = new HorizontalLayout(plus, minus);
+        minus.addThemeVariants(ButtonVariant.LUMO_ICON);
+        plus.addThemeVariants(ButtonVariant.LUMO_ICON);
+        minus.addClickListener(event -> minus());
+        plus.addClickListener(event -> plus(quantity.getTabIndex()));
+        add(productName, price, quantity, plusAndMinus, type, buttons);
         binder.bindInstanceFields(this);
         this.orderMainView = orderMainView;
+    }
+
+    private void minus() {
+    }
+
+    private Integer plus(Integer m) {
+        service.addQuantity(plus(m));
+        orderMainView.refresh();
+        return m;
     }
 
     private void order() {
--- src/main/java/com/kodilla/restaurantfrontend/service/ProductService.java
+++ src/main/java/com/kodilla/restaurantfrontend/service/ProductService.java
@@ -33,19 +33,21 @@ public class ProductService {
 
     private Set exampleData() {
         Set<Product> products = new HashSet<>();
-       products.add(new Product("Pizza", "32","true", "1", ProductType.MAIN_COURSE));
-       products.add(new Product("Pomodoro", "12","true", "1", ProductType.SOUP));
-       products.add(new Product("Ice", "22","true", "2", ProductType.DESSERT));
-       products.add(new Product("Coca-cola", "6","true", "2", ProductType.DRINKS));
+        products.add(new Product("Pizza", "32", "true", "1", ProductType.MAIN_COURSE));
+        products.add(new Product("Pomodoro", "12", "true", "1", ProductType.SOUP));
+        products.add(new Product("Ice", "22", "true", "2", ProductType.DESSERT));
+        products.add(new Product("Coca-cola", "6", "true", "2", ProductType.DRINKS));
         return products;
     }
+
     public Set findByProductName(String productName) {
-        return  products.stream()
+        return products.stream()
                 .filter(product -> product.getProductName().contains(productName))
                 .collect(Collectors.toSet());
     }
+
     public Set findByProductPrice(String price) {
-        return  products.stream()
+        return products.stream()
                 .filter(product -> product.getPrice().contains(price))
                 .collect(Collectors.toSet());
     }
@@ -56,6 +58,11 @@ public class ProductService {
 
     public void delete(Product product) {
         this.products.remove(product);
+    }
+
+    public Integer addQuantity(Integer m) {
+        products.stream().map(n -> n.getQuantity()).map(n -> n + 1).collect(Collectors.toList());
+        return getProducts().size();
     }
 
 }
